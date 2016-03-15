e3665c2b1c808c078a25ae1b40e8830bc8e5cb4b
Burak Enez <burakenez@gmail.com>
2016-03-15 02:53:58 +0200

    Updates

---
+++ Libraries/pic18f4520_config.h
@@ -0,0 +1,118 @@
+/**
+ * @author  Burak Enez
+ * @email   burakenez@gmail.com
+ * @website http://burakenez.org
+ * @version v1.0
+ * @ide     MPLAB X IDE
+ * @license GNU GPL v3
+ * @brief   Hardware Configurations for PIC18F4520 Development Board
+ *
+ * ----------------------------------------------------------------------
+ *  Copyright (C) Burak Enez, 2016
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *   
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * ----------------------------------------------------------------------
+ */
+
+#ifndef _PIC18F4520_CONFIG_H
+#define	_PIC18F4520_CONFIG_H 100
+
+/* C++ detection */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* PIC18F4520 Configuration Bit Settings */
+/* 'C' source line config statements */
+
+#include <xc.h>
+
+#define _XTAL_FREQ 8000000
+
+/* #pragma config statements should precede project file includes. */
+/* Use project enums instead of #define for ON and OFF. */
+
+/* CONFIG1H 0x02 */
+#pragma config OSC = HSPLL      // Oscillator Selection bits (HS oscillator, PLL enabled (Clock Frequency = 4 x FOSC1))
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
+/* CONFIG2L 0x13 */
+#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bits (Brown-out Reset enabled and controlled by software (SBOREN is enabled))
+#pragma config BORV = 2         // Brown Out Reset Voltage bits ()
+
+/* CONFIG2H 0x1E */
+#pragma config WDT = OFF        // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
+
+/* CONFIG3H 0X81 */
+#pragma config CCP2MX = PORTC   // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = OFF     // PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
+#pragma config LPT1OSC = OFF    // Low-Power Timer1 Oscillator Enable bit (Timer1 configured for higher power operation)
+#pragma config MCLRE = ON       // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+
+/* CONFIG4L 0x81 */
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config LVP = OFF        // Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+
+/* CONFIG5L 0x0F */
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000800-001FFFh) not code-protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 (002000-003FFFh) not code-protected)
+#pragma config CP2 = OFF        // Code Protection bit (Block 2 (004000-005FFFh) not code-protected)
+#pragma config CP3 = OFF        // Code Protection bit (Block 3 (006000-007FFFh) not code-protected)
+
+/* CONFIG5H OXC0 */
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
+
+/* CONFIG6L 0X0F */
+#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000800-001FFFh) not write-protected)
+#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (002000-003FFFh) not write-protected)
+#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (004000-005FFFh) not write-protected)
+#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (006000-007FFFh) not write-protected)
+
+/* CONFIG6H 0XE0 */
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot block (000000-0007FFh) not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+
+/* CONFIG7L 0X0F */
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000800-001FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (002000-003FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (004000-005FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (006000-007FFFh) not protected from table reads executed in other blocks)
+
+/* CONFIG7H 0X40 */
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) not protected from table reads executed in other blocks)
+
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
+
+/* C++ detection */
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PIC18F4520_CONFIG_H */
---
+++ PIC18F4520-XC8-LED.X/main.c
@@ -0,0 +1,38 @@
+/**
+ * MPLABX IDE project example for blinking LED
+ * 
+ * @author   Burak Enez
+ * @email    burakenez@gmail.com
+ * @website  http://burakenez.org
+ * @ide      MPLAB X IDE
+ * @compiler XC8
+ * @license  GNU GPL v3
+ */
+
+/* Include core modules */
+#include <stdio.h>
+#include <stdlib.h>
+/* Include my libraries here */
+#include "pic18f4520_config.h"
+#include "pic18f4520_delay.h"
+
+int main(void) {
+    /* Initializes RC1 pin as output (LED1) */
+    TRISCbits.RC1 = 0;
+    
+    /* Loop forever */
+    while(1) {
+        
+        /* Sets RC1 pin (LED1) */
+        LATCbits.LATC1 = 1;
+        
+        /* 1000ms delay */
+        Delayms(1000);
+
+        /* Resets RC1 pin (LED1) */
+        LATCbits.LATC1 = 0;
+        
+        /* 1000ms delay */
+        Delayms(1000);
+    }
+}
