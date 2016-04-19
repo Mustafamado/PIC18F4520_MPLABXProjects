1bc940d52ead95e1f2d3a15efd192bc26bcd2eba
Burak Enez <burakenez@gmail.com>
2016-04-19 11:48:02 +0300

    Update main.c

--- PIC18F4520-XC8-TMR0I.X/main.c
+++ PIC18F4520-XC8-TMR0I.X/main.c
@@ -8,7 +8,6 @@
  * @compiler XC8
  * @license  GNU GPL v3
  */
-
 /* Include core modules */
 #include <xc.h>
 #include <plib/timers.h>
@@ -17,49 +16,41 @@
 #include "pic18f4520_delay.h"
 
 /* fcmd = fosc/4 = 32MHz/4 = 8Mhz
- * Tcmd = 1/fcmd = 1/8MHz = 1/(8*10^6) s = 0.125 us
+ * Tcmd = 1/fcmd = 1/8MHz = 1/(8*10^6) s = 0.125 us = 0.125*10^-6 s
  * TMR0_value = 2^TMR0bit - (DesiredInterruptTime/(Tcmd*Prescaler))
- * Prescaler = 16
+ * Prescaler = 128
  * TMR0bit = 16
- * DesiredInterruptTime = 1ms
- * TMR0_value = 65036
+ * DesiredInterruptTime = 1s
+ * TMR0_value = 3036
  */
 
-int TMR0_value = 65036; /* Required value for 1 second Timer0 Interrupt */
-int millis = 0;
-
-/* LED1 */
-#define TRIS_LED1   TRISCbits.RC1
-#define LAT_LED1    LATCbits.LATC1
-/* LED2 */
-#define TRIS_LED2   TRISCbits.RC2
-#define LAT_LED2    LATCbits.LATC2
+int TMR0_value = 3036; /* Required value for 1 second Timer0 Interrupt */
 
 void main(void) {
     
-    TRIS_LED1 = 0;
-    LAT_LED1 = 0;
+    TRISCbits.RC1 = 0;
+    LATCbits.LATC1 = 0;
     /* Configure Timer0 */
     OpenTimer0(TIMER_INT_ON &  /* Enable TIMER Interrupt */
                T0_16BIT &      /* Timer0 is configured as an 16-bit timer/counter */
                T0_SOURCE_INT & /* Internal instruction cycle clock (CLKO) acts as source of clock */
-               T0_PS_1_16);   /* 1:256 Prescale value */
-
+               T0_PS_1_128);   /* 1:128 Prescale value */
+               
     WriteTimer0(TMR0_value);
 
     INTCONbits.GIE = 1;
-    while (1);
+    
+    while(1) { 
+        
+    }
 }
 
 void interrupt Interrupt(void) {
     /* If Timer flag is set & Interrupt is enabled */
     if(INTCONbits.TMR0IF==1) {
-        INTCONbits.TMR0IF = 0;    /*  Clear the interrupt flag  */
-        WriteTimer0(TMR0_value);
-        if(++millis == 1000) {            
-            LAT_LED1 =~ LAT_LED1;
-            millis = 0;
-        }
+        INTCONbits.TMR0IF = 0;   /* Clear the interrupt flag */
+        WriteTimer0(TMR0_value); /* Update TMR0 value */
+        LATCbits.LATC1 = ~LATCbits.LATC1; 
     }
 }
 
