5666b46ec2cbbb7dd55a5cea6cbcd7fd237b4cac
Burak Enez <burakenez@gmail.com>
2016-04-19 15:27:36 +0300

    Update main.c

--- PIC18F4520-XC8-BUTTON.X/main.c
+++ PIC18F4520-XC8-BUTTON.X/main.c
@@ -16,21 +16,16 @@
 #include "pic18f4520_delay.h"
 
 int main(void) {
-
-    /* Initializes RC1 pin as output (LED1) */
-    TRISCbits.RC1 = 0; 
-    /* Initializes RB0 pin as input (KEY1) */
-    TRISBbits.RB0 = 1;
-    /* Initializes RB1 pin as input (KEY2) */
-    TRISBbits.RB1 = 1; 
-
+    TRISCbits.RC1 = 0; // Initializes RC1 pin as output (LED1)
+    TRISBbits.RB0 = 1; // Initializes RB0 pin as input (KEY1)
+    TRISBbits.RB1 = 1; // Initializes RB1 pin as input (KEY2)
+    
     while(1) {
-        
-        if (PORTBbits.RB0 == 0) /* If RB0 pin is equal to 0 (If KEY1 is pressed) */
-            LATCbits.LATC1 = 1; /* Sets RC1 pin (LED1) */
-
-        if (PORTBbits.RB1 == 0) /* If RB1 pin is equal to 0 (if KEY2 is pressed) */
-            LATCbits.LATC1 = 0; /* Resets RC1 pin (LED1) */
-
+        // If RB0 pin is equal to 0 (if KEY1 is pressed)
+        if (PORTBbits.RB0 == 0) 
+            LATCbits.LATC1 = 1; // Sets RC1 pin (LED1)
+        // If RB1 pin is equal to 0 (if KEY2 is pressed)
+        if (PORTBbits.RB1 == 0) 
+            LATCbits.LATC1 = 0; // Resets RC1 pin (LED1)
     }
 }
